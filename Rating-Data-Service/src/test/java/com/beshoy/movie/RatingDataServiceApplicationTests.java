ec9b55c9d1b1a2abe46f830d8445a6b932918b3f
b.gerges <b.gerges@egyptianbanks.net>
2020-04-12 21:35:31 +0200

    Initial commit

---
+++ Rating-Data-Service/src/test/java/com/beshoy/movie/RatingDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beshoy.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
